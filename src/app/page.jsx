9d19c0d55e8676b7b26cd3917a63bb0b10bb8bf2
kawsar9990 <kawsar158464@gmail.com>
2025-10-22 22:33:36 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+import FirstSlider from "./home/firstslide";
+
+export default function Home() {
+  return (
+     <div className="pt-30 xl:pt-35" >
+
+    <div className="">
+      <FirstSlider />
+    </div>
+
+
+    </div>
+  );
+}
